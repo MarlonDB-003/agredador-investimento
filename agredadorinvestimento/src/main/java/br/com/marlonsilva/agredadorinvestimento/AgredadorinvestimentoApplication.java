f0de0a7bfc9f0a4747e09a2c74fd5d7373adee3e
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-20 09:12:09 -0400

    criação do projeto

---
+++ agredadorinvestimento/src/main/java/br/com/marlonsilva/agredadorinvestimento/AgredadorinvestimentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.marlonsilva.agredadorinvestimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgredadorinvestimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgredadorinvestimentoApplication.class, args);
+	}
+
+}
---
+++ agredadorinvestimento/src/test/java/br/com/marlonsilva/agredadorinvestimento/AgredadorinvestimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marlonsilva.agredadorinvestimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgredadorinvestimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
